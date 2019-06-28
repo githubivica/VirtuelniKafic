c981af7d7be8116294d9137f808fa8ef0108257a
Iva <Iva@Iva-PC>
2019-06-29 01:52:39 +0200

    admina sam podesio od kuce

--- VirtuelniKafic/src/admin/Admin.java
+++ VirtuelniKafic/src/admin/Admin.java
@@ -2,4 +2,14 @@ package admin;
 
 public class Admin {
 
+	private final String adminUserName = "admin";
+	private final String adminPassword = "admin";
+	
+	public String getAdminUserName() {
+		return adminUserName;
+	}
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+	
 }
